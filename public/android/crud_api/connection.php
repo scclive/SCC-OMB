dfbcc519e9970ff0355632bfed094e41fad479c3
scclive <61845177+scclive@users.noreply.github.com>
2020-07-13 02:00:52 +0500

    env

--- public/android/crud_api/connection.php
+++ public/android/crud_api/connection.php
@@ -1,11 +1,13 @@
 <?php
 
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db = "laraquiz";
+$user = "ohhmybug_admin";
+$pass = "T$L;~?0+=~2Z";
+$db = "ohhmybug_laraquiz";
 
 $con = mysqli_connect($host,$user,$pass, $db);
+if($con) echo "Success";
+else echo "Failed";
 
 // if($con){
 //   echo "Koneksi Berhasil";
