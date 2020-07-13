da8577c1b511c17120afa06c6fb89024130e7c8c
scclive <61845177+scclive@users.noreply.github.com>
2020-07-13 09:06:27 +0500

    1f

--- public/android/crud_api/connection.php
+++ public/android/crud_api/connection.php
@@ -6,8 +6,6 @@ $pass = "scc@scc.ohhmybug.com123";
 $db = "ohhmybug_laraquiz";
 
 $con = mysqli_connect($host,$user,$pass, $db);
-if($con) echo "Success";
-else echo "Failed";
 
 // if($con){
 //   echo "Koneksi Berhasil";
